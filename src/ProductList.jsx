5c59ff2e79b49c280ebcd51c28d3283392019d01
Emmanuel-Bolanos <emmanuel.bolanos@theksquaregroup.com>
2024-11-13 12:08:42 -0500

    update styles

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -282,7 +282,7 @@ function ProductList() {
                         <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
                             <h1 className='cart'>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
-                                0
+                                <div className='cart_quantity_count'>0</div>
                             </h1>
                         </a>
                     </div>
@@ -292,14 +292,14 @@ function ProductList() {
                 <div className="product-grid">
                     {plantsArray.map((category, idx) => (
                         <div key={idx}>
-                            <h2>{category.category}</h2>
+                            <h1 className='product-category'>{category.category}</h1>
                             <div className='product-list'>
                                 {category.plants.map((item, itemIdx) => (
                                     <div key={itemIdx} className='product-card'>
-                                        <img className='product-image' src={item.image} alt={item.name} />
                                         <div className='product-title'>{item.name}</div>
+                                        <img className='product-image' src={item.image} alt={item.name} />
+                                        <div className='product-price'>{item.cost}</div>
                                         <div className='product-description'>{item.description}</div>
-                                        <div className='product-cost'>{item.cost}</div>
                                         <button  className={productButtonClasses(item.name)} disabled={addedToCart[item.name]} onClick={() => handleAddToCart(item)}>Add to Cart</button>
                                     </div>
                                 ))}
